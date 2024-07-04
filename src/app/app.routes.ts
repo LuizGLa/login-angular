9b5500ddfb72c78fcce44ecdc7e5b647a7ec0ebf
leonardovbdo <odranoelrubi@gmail.com>
2024-07-04 12:02:53 -0300

    Ajustes de toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'cadastrar',
     component: CadastrarComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
