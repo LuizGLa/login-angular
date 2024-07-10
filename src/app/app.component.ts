d27af5d80148ab691d1829514f6fc79bf4ba95ea
Luiz Gustavo <luizgustavolima200@gmail.com>
2024-07-10 07:18:53 -0300

    Exibição de usuario navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { filter } from 'rxjs';
+import { UsuarioService } from './services/usuario.service';
+import { Usuario } from './models/usuario.model';
 
 
 @Component({
@@ -33,10 +35,19 @@ import { filter } from 'rxjs';
 })
 export class AppComponent implements AfterViewInit {
   title = 'ocorre-map';
-
+  user: any;
+  userId: string | null = null;
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
-  constructor(private breakpointObserver: BreakpointObserver, public router: Router) { }
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public router: Router,
+    private usuarioService: UsuarioService
+  ) {
+    if (typeof sessionStorage !== 'undefined') {
+      this.userId = sessionStorage.getItem('userId');
+    }
+  }
 
   ngAfterViewInit(): void {
     this.breakpointObserver.observe([Breakpoints.Handset])
@@ -59,6 +70,17 @@ export class AppComponent implements AfterViewInit {
         this.sidenav.open();
       }
     });
+
+    if (this.userId) {
+      this.usuarioService.getUsuarioPorId(this.userId).subscribe(
+        (usuario: any) => {
+          this.user = usuario;
+        },
+        (error) => {
+          console.error('Error fetching user details:', error);
+        }
+      );
+    }
   }
 
   onToggleSidenav() {
